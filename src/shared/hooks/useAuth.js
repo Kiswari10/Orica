1c0e178fefa756f5867abf1d32e89b9e77e1818d
Jorge Yovera <jotayh18@gmail.com>
2024-01-09 10:07:41 -0500

    feat(Login): add crypto-js

--- src/shared/hooks/useAuth.js
+++ src/shared/hooks/useAuth.js
@@ -1,28 +1,43 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-//import CryptoJS from "crypto-js";
+import CryptoJS from "crypto-js";
 
 const useAuth = () => {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
+  const encryptPassword = (pass) => {
+    let encrypt = CryptoJS.AES.encrypt(pass, "@encryptpass").toString();
+    return encrypt;
+  };
+
+  /*const decryptPassword = (pass) => {
+    let bytes = CryptoJS.AES.descrypt(pass, "@encryptpass");
+    let descrypt = bytes.toString(CryptoJS.enc.Utf8);
+    return descrypt;
+  };*/
+
   const login = (userData) => {
     // lógica de autenticación
-    setUser(userData);
-    localStorage.setItem('user', JSON.stringify(userData));
+    // cifrado
+    const pass = encryptPassword(userData?.password);
+    const data = {username: userData?.username, password: pass}
 
-    navigate('/solution-generation', {
-			replace: true,
-			state: {
-				logged: true,
-				name: userData?.username,
-			},
-		});
+    setUser(data);
+    localStorage.setItem("user", JSON.stringify(data));
+
+    navigate("/solution-generation", {
+      replace: true,
+      state: {
+        logged: true,
+        name: userData?.username,
+      },
+    });
   };
 
   const logout = () => {
     setUser(null);
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
   };
 
   useEffect(() => {
